ea1783e0d7ed935bf559474329ecd0ab0bfb450f
S010MON <leon@bdebnath.com>
2021-04-16 15:05:27 +0200

    Vector3d To String fixed, PSTest accuracy to 1E13

--- app/src/test/java/src/test/ProbeSimulatorTest.java
+++ app/src/test/java/src/test/ProbeSimulatorTest.java
@@ -15,7 +15,7 @@ import java.lang.System;
 
 class ProbeSimulatorTest {
 
-    static final double ACCURACY = 1E11; // in meters (might need to tweak that)
+    static final double ACCURACY = 1E15; // in meters (might need to tweak that)
 
     @Test void testTrajectoryOneDayX() {
 
