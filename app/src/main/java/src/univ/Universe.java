87b25e14ff9db560e38368a5e69cce2bd1536b9d
S010MON <leon@bdebnath.com>
2021-04-24 20:13:05 +0200

    Reconfigured to load from file

--- app/src/main/java/src/univ/Universe.java
+++ app/src/main/java/src/univ/Universe.java
@@ -6,6 +6,9 @@ import src.peng.Vector3dInterface;
 import src.config.ConfigFileManager;
 import src.config.DataFileManager;
 import src.config.SimulationSettings;
+import src.peng.NewtonGravityFunction;
+import src.peng.ODEFunctionInterface;
+import src.peng.RungeKutta4th;
 import src.peng.State;
 
 import java.time.LocalDate;
@@ -15,156 +18,84 @@ import java.util.ArrayList;
 
 public class Universe
 {
-    public  CelestialBody[] startVariables; 
-    public  CelestialBody[][] universe;
+    public CelestialBody[] startVariables; 
+    public CelestialBody[][] universe;
 	public LocalDateTime startTime;
 	public LocalDateTime endTime;
 	public int noOfSteps;
-
+	public double stepSize;
+	public int stepSizeNanoSec;
+	public double[] masses;
 	public String[] wayPoints;
 	
-	// If known settings are provided, run this 
     public Universe(SimulationSettings settings)
     {
-    	this.startTime = settings.startTime;
-    	this.endTime = settings.endTime;
-    	this.noOfSteps = settings.noOfSteps;
-    	this.wayPoints = settings.wayPoints;
-    	this.startVariables = settings.celestialBodies;
+    	startTime = settings.startTime;
+    	endTime = settings.endTime;
+    	noOfSteps = settings.noOfSteps;
+    	startVariables = settings.celestialBodies;
+		stepSize = settings.stepSize;
+		stepSizeNanoSec = (int) (stepSize*1E9);
+     	masses = new double[startVariables.length];
+    	for(int i = 0; i < startVariables.length; i++)
+    	{
+    		masses[i] = startVariables[i].mass;
+    	}
     	
     	universe = new CelestialBody[startVariables.length][noOfSteps];
-    	DataFileManager dataBase = new DataFileManager();
-     	if(dataBase.query(settings))
-     	{
-	    	try
-			{
-				
-				CelestialBody[] temp = config.load("UniverseConfig");
-				
-			}
-			catch (Exception e)
-			{
-				System.out.println("Unable to load config file");
-				e.printStackTrace();
-			}
-     	}
-     	else
-     	{
-     		
-     	}
-    }
-    
-    // If a config file is loaded, run this 
-    public Universe(String configFileName)
-    {
     	try
 		{
-			ConfigFileManager config = new ConfigFileManager();
-			CelestialBody[] temp = config.load("UniverseConfig");
-			U = new CelestialBody[temp.length+1];
-			for(int i = 0; i < temp.length; i++)
-			{
-				U[i] = temp[i];
-			}
+    		universe = DataFileManager.load(settings);
 		}
 		catch (Exception e)
 		{
-			System.out.println("Unable to load config file");
-			e.printStackTrace();
-		}
+			System.out.println("Unable to load config file/n");
+			universe = generateNewUniverse();
+     	}
     }
-    
-    // purely for tests 
-    public Universe(CelestialBody[] universe)
+     
+    private CelestialBody[][] generateNewUniverse()
     {
-    	this.universe = universe;
+		StateInterface initialState = convertToState(startVariables);
+		ODEFunctionInterface function = new NewtonGravityFunction(masses);			
+		RungeKutta4th solver = new RungeKutta4th();									
+		StateInterface[] states = solver.solve(function, initialState, stepSize*noOfSteps, stepSize);			
+		return convertToCelestialBody(states);														
     }
     
-    
-    //public getState(int step)
-    
-    	// Get the CBs at the time step
-    	// Convert to a state
-    
-    // public setState(StateInterface[] state, int step)
-    
-    	// take in the stateInterface and set each as a CB
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public StateInterface initialState()
+    public StateInterface convertToState(CelestialBody[] bodies)
     {
         ArrayList<Vector3d> velocity = new ArrayList<Vector3d>();
         ArrayList<Vector3d> position = new ArrayList<Vector3d>();
 
-        for(int i=0; i< U.length; i++)
+        for(int i = 0; i < bodies.length; i++)
         {
-            velocity.add(U[i].velocity);
-            position.add(U[i].location);
+            velocity.add(bodies[i].velocity);
+            position.add(bodies[i].location);
         }
         return new State(velocity, position);
     }
-    
-    public double[] getMasses()
-    {
-    	double[] masses = new double[U.length];
-    	for(int i = 0; i < masses.length; i++)
-    	{
-    		masses[i] = U[i].mass;
-    	}
-    	return masses;
-    }
         
-    public void update(StateInterface[] States)
-    {
-    	//LocalDateTime dateTime = LocalDateTime.of(date, time);
-    	
-    	U2 = new CelestialBody[U.length][States.length];
-    	for(int i=0; i< States.length; i++)
-        {
-            //Cast each object into a State object
-            State currentState = (State)States[i];
-
-            //Iterate through each planet contained in a single state
-            for(int j=0; j< currentState.velocity.size(); j++)
+    public CelestialBody[][] convertToCelestialBody(StateInterface[] stateInterfaces)
+    {  	
+    	State[] states = (State[]) stateInterfaces;
+    	CelestialBody[][] bodies = new CelestialBody[universe.length][universe[0].length];
+    	for(int i = 0; i < states.length; i++)
+        {            
+            LocalDateTime dateTime = startTime;
+    		for(int j = 0; j < states[i].velocity.size(); j++)
             {
-                U2[j][i] = U[j].updateCopy(currentState.position.get(j),
-                						   currentState.velocity.get(j), 
-                						   dateTime);
+    			bodies[j][i] = startVariables[j].updateCopy(states[i].position.get(j),
+                										states[i].velocity.get(j), 
+                						   				dateTime);
             }
-            dateTime.plusNanos(stepSize);
+            dateTime.plusNanos((long) stepSizeNanoSec);
         }
-    	save();
+    	return bodies;
     }
     
     public void saveToFile()
     {
-    	DataFileManager fileMngr = new DataFileManager();
-    	for(int i = 0; i < U.length; i++)
-    	{
-    		fileMngr.overwrite(U2[i]);
-    	}
+    	DataFileManager.overwrite(universe);
     }
 }
