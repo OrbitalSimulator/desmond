5d4f7773d54f1985c7df9a85ee4d4fccc5393bfc
Cagancancacan <72268327+Cagancancacan@users.noreply.github.com>
2021-04-26 05:26:50 +0300

    Improvements/ problem with some methods
    There is a small issue where im not sure how to use methods to manipulate the state in order to get the acceleration, after that though, should be all done.

--- app/src/main/java/src/peng/VerletSolver.java
+++ app/src/main/java/src/peng/VerletSolver.java
@@ -5,7 +5,8 @@ import java.lang.Math;
 public class VerletSolver implements ODESolverInterface	//find position x_n+1 using x_n and x_n-1
 {
 	public static final boolean DEBUG = false;
-	
+	private StateInterface[] results;
+	private int index;
 	
     /**
      * Solve the differential equation by taking multiple steps of equal size, starting at time 0.
@@ -19,18 +20,21 @@ public class VerletSolver implements ODESolverInterface	//find position x_n+1 us
      */
     public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h)
     {
-        StateInterface[] results = new StateInterface[(int)Math.ceil((tf/h)+1)];	// Instantiate new StateInterface array of size Round(tf/h)+1
-        int index = 0;																// Index to track additions to results
+        results = new StateInterface[(int)Math.ceil((tf/h)+1)];	// Instantiate new StateInterface array of size Round(tf/h)+1
+        index = 0;																// Index to track additions to results
         results[index] = y0;														// Record initial state within results:
         index++;
         StateInterface currentState = y0;											// State variable to allow updates within the loop
         double t = 0;																// Instantiate starting time at t=0;
         
         //want to set position_1, so that we can use that to find the next positions
-        //find acceleration using f.newtonGravity(...) at steps 
-        results[index] = 
+        //find acceleration using f.call(...) at steps 
+        
         while(t < tf)
         {
+        	if (index==1) {	//special case for x1, where x1 = x0 +v0*h + 1/2*A(x0)*h^2
+        		results[index] = 
+        	}
             currentState = step(f, t, currentState, h);								// Determine new current state through a single step of the Euler method
             results[index] = currentState;											// Add new state to the results array
             index++;
@@ -77,23 +81,23 @@ public class VerletSolver implements ODESolverInterface	//find position x_n+1 us
     }
     
     /**
-     * Update rule for one step.
+     * Update state for one step (for verlet method).
      * Essentially adding the calculated changes (In acceleration, position)
+     * Assuming for steps past x1, so using x_n+1 = 2*xn - x_n-1 + A(xn)*h^2
      * @param   f   the function defining the differential equation dy/dt=f(t,y)
      * @param   t   the time
      * @param   y   the state
      * @param   h   the step size
      * @return  the new state after taking one step
      */
-    public StateInterface verletStep(ODEFunctionInterface f, double t, StateInterface y, double h)
+    public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h)
     {
     	//need to get x_n-1 and x_n, as well as the acceleration being applied, can then find x_n+1
-    	
-        return ;
-    }
 
-    public StateInterface accelerationFunction(ODEFunctionInterface f, double t, StateInterface y, double h) {
-    	
+    	y = ((State) y).scale(2.0);										//the 2*xn part
+    	y = ((State) y).add(((State) results[index-1]).scale(-1.0));	//the 2*xn - x_n-1
+    	y = y.addMul((h*h), f.call(t, y.call(t,y)));					//<-----------sorta issue
+        return (StateInterface) y;
     }
 }
 
