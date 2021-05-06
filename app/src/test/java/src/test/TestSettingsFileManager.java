8c3d7d275cc312eced77a35dee5c1b5308439bd5
J3r0ms <72269638+J3r0ms@users.noreply.github.com>
2021-05-06 18:38:08 +0200

    Update TestSettingsFileManager.java

--- app/src/test/java/src/test/TestSettingsFileManager.java
+++ app/src/test/java/src/test/TestSettingsFileManager.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 
 import src.conf.SettingsFileManager;
 import src.conf.SimulationSettings;
+import src.peng.Vector3dInterface;
+import src.peng.Vector3d;
 
 class TestSettingsFileManager 
 {
@@ -19,6 +21,17 @@ class TestSettingsFileManager
 	LocalTime etime = LocalTime.parse("00:00:00");
 	LocalDate edate = LocalDate.parse("2022-04-01");
 	LocalDateTime endDateTime = LocalDateTime.of(edate, etime);
+	Vector3dInterface pos = new Vector3d(-1.471922101663588e+11, -2.860995816266412e+10, 8.278183193596080e+06);
+	Vector3dInterface vel = new Vector3d(21240.816883792304, -57621.75558355437, -268.76563663220065);
+	Vector3d location = new Vector3d(6.047855986424127e+06,-6.801800047868888e+10,-5.702742359714534e+09);
+	Vector3d velocity = new Vector3d(3.892585189044652e+04, 2.978342247012996e+03,-3.327964151414740e+03);
+	double mass = 3.302e23;
+	double radius = 2440e3;
+	String image = "/src/main/java/src/misc/mercuryScaled.png";
+	String icon = "/src/main/java/src/misc/mercuryIcon.png";
+	LocalTime cbTime = LocalTime.parse("00:00:00");
+	LocalDate cbDate = LocalDate.parse("2021-04-01");
+	LocalDateTime cbDateTime = LocalDateTime.of(cbDate, cbTime);
 	
 	@Test
 	void test() 
@@ -28,8 +41,17 @@ class TestSettingsFileManager
 			
 			assertEquals(startDateTime, settings.startTime);
 			assertEquals(endDateTime, settings.endTime);
-			assertEquals(100000, settings.noOfSteps);
-			assertEquals(500, settings.stepSize);
+			assertEquals(11712, settings.noOfSteps);
+			assertEquals(2700, settings.stepSize);
+			assertEquals(pos, settings.probeStartPosition);
+			assertEquals(vel, settings.probeStartVelocity);
+			assertEquals(location, settings.celestialBodies[1].location);
+			assertEquals(velocity, settings.celestialBodies[1].velocity);
+			assertEquals(mass, settings.celestialBodies[1].mass);
+			assertEquals(radius, settings.celestialBodies[1].radius);
+			assertEquals(image, settings.celestialBodies[1].image);
+			assertEquals(icon, settings.celestialBodies[1].icon);
+			assertEquals(cbDateTime, settings.celestialBodies[1].time);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
