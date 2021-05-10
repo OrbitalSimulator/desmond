7003e6ccf1197410dc4746a13f19176809c1294e
Mustafa Alp Erçıkan <72265922+ercikanalp@users.noreply.github.com>
2021-05-10 14:08:16 +0200

    Update App.java

--- app/src/main/java/src/App.java
+++ app/src/main/java/src/App.java
@@ -1,24 +1,19 @@
 package src;
 
 import java.io.IOException;
-
-import src.conf.SettingsFileManager;
-import src.conf.SimulationSettings;
-import src.peng.TrajectoryPlanner;
-import src.univ.Universe;
-import src.visu.Visualiser;
+import src.univ.CelestialBody;
+import src.visu.SetupMenu;
 
 public class App 
 {
 	public static void main(String[] args) 
     {
-		SimulationSettings settings;
+
 		try 
 		{
-			settings = SettingsFileManager.load();
-			Universe universe = new Universe(settings);
-			Visualiser visualiser = new Visualiser(universe.universe);		
-			visualiser.addTrajectory(TrajectoryPlanner.plot(universe, settings));
+
+			CelestialBody[] U=new CelestialBody[0];
+			new SetupMenu(U);
 		}
 		catch(IOException e)
 		{
