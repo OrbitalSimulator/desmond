0ce52bad12fe77c9bc2b3e411e69c6c96cd30234
S010MON <leon@bdebnath.com>
2021-05-14 20:39:30 +0200

    Implements calculating start location

--- app/src/main/java/src/traj/TrajectoryPlanner.java
+++ app/src/main/java/src/traj/TrajectoryPlanner.java
@@ -8,6 +8,15 @@ public abstract class TrajectoryPlanner
 {	
 	public static Vector3d[] plot(Universe universe, SimulationSettings settings)
 	{					
+		// Lookup start planet
+		//	-> V = V of start planet
+		// Lookup end planet
+		//	-> P = startPlanet(t = 0).closestLaunchPoint(endPlanet(t = end)
+		
+		
+		Vector3d titan = universe.universe[8][universe.universe.length-1].location;
+		universe.universe[3][0].closestLaunchPoint(titan);
+		
 		RouteController rc = new RouteController(universe, "titan", settings);
 		return rc.getTrajectory();
 	}
