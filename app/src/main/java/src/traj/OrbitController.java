cd5c64e729a5f5b18f2bfbbf9cdb665868434c0a
S010MON <leon@bdebnath.com>
2021-05-18 10:27:18 +0200

    Target changed to string

--- app/src/main/java/src/traj/OrbitController.java
+++ app/src/main/java/src/traj/OrbitController.java
@@ -5,7 +5,7 @@ import src.univ.Universe;
 public class OrbitController extends GuidanceController
 {
 
-	public OrbitController(Universe universe, String target)
+	public OrbitController(Universe universe, int target)
 	{
 		super(universe, target);
 	}
