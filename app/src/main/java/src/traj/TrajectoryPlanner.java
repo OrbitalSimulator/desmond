855c542bdff32eca11b777e638321d80735a18a1
S010MON <leon@bdebnath.com>
2021-05-18 10:29:03 +0200

    Addition of seperate legs

--- app/src/main/java/src/traj/TrajectoryPlanner.java
+++ app/src/main/java/src/traj/TrajectoryPlanner.java
@@ -8,19 +8,25 @@ public abstract class TrajectoryPlanner
 {	
 	public static Vector3d[] plot(Universe universe, SimulationSettings settings)
 	{					
-		int startPlanet = 7;
-		int endPlanet = 9;
+		int startPlanet = 3;
+		int endPlanet = 8;
+		
+		SimulationSettings leg1 = settings.copy();
+		leg1.noOfSteps = 2700;
+		SimulationSettings leg2 = settings.copy();
+		leg2.noOfSteps = 2700;
+		leg2.stepOffset = 2700;
 		
 		// Lookup end planet
 		//	-> P = startPlanet(t = 0).closestLaunchPoint(endPlanet(t = end)
-		Vector3d end = universe.universe[startPlanet][universe.universe.length-1].location;
+		Vector3d end = universe.universe[endPlanet][universe.universe.length-1].location;
 		
 		// Lookup start planet
 		//	-> V = V of start planet
-		settings.probeStartPosition = universe.universe[endPlanet][0].closestLaunchPoint(end);
-		settings.probeStartVelocity = universe.universe[endPlanet][0].velocity;
+		settings.probeStartPosition = universe.universe[startPlanet][0].closestLaunchPoint(end);
+		settings.probeStartVelocity = universe.universe[startPlanet][0].velocity;
 		
-		RouteController rc = new RouteController(universe, "titan", settings);
+		RouteController rc = new RouteController(universe, 8, settings);
 		return rc.getTrajectory();
 	}
 	
