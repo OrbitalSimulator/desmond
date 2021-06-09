fb7d032a911429c7105be2af44430e1cf6007209
Mustafa Alp Erçıkan <72265922+ercikanalp@users.noreply.github.com>
2021-06-09 10:25:25 +0200

    Fixed error in test class.

--- app/src/test/java/src/test/ErrorTest.java
+++ app/src/test/java/src/test/ErrorTest.java
@@ -3,20 +3,19 @@ package src.test;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static src.solv.Error.absoluteError;
-import static src.solv.Error.relativeError;
+import src.solv.Error;
 
 class ErrorTest
 {
     @Test
     void testAbsolute()
     {
-        assertEquals(1.99-1.01, absolute(1.01,1.99));
+        assertEquals(1.99-1.01, Error.absolute(1.01,1.99));
     }
 
     @Test
     void testRelative()
     {
-        assertEquals((1.99-1.01)/1.99, relative(1.99,1.01));
+        assertEquals((1.99-1.01)/1.99, Error.relative(1.99,1.01));
     }
 }
