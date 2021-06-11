2accfa3e4f1c5d8fdcf17e82cb595c1ec8e3aaf6
Cagancancacan <can.idil1@gmail.com>
2021-06-11 12:35:44 +0300

    Fixed free fall function to match NASA's given formula more closely.

--- app/src/main/java/src/peng/FreeFallFunction.java
+++ app/src/main/java/src/peng/FreeFallFunction.java
@@ -8,6 +8,9 @@ public class FreeFallFunction implements ODEFunctionInterface {
 	
 	private LanderSettings settings;
 	public static final double GRAVITY = 6.67430e-11;
+	private double r = 147000;
+	private double dragCoeff = 0.55;
+	private double area = settings.module.length * settings.module.width;
 
 	public FreeFallFunction(LanderSettings settings) {
 		this.settings = settings;
@@ -25,7 +28,7 @@ public class FreeFallFunction implements ODEFunctionInterface {
         State2d stateInfo = (State2d)y;                                      //Cast y into State object to access information
 
         /*1.Calculate resultant sum acceleration by gravity*/
-        ArrayList<Vector2d> cvG = newtonGravity(stateInfo, settings.module.body.mass);
+        ArrayList<Vector2d> cv = freeFall(stateInfo, settings.module.body.mass);
         
         /*2. Calculate the resultant change in position by gravity*/
         ArrayList<Vector2d> cp = new ArrayList<Vector2d>();
@@ -34,18 +37,8 @@ public class FreeFallFunction implements ODEFunctionInterface {
             cp.add(stateInfo.velocity.get(i));
         }
         
-        /*3. Calculate the total resultant acceleration by drag and gravity*/
-        ArrayList<Vector2d> cvT = drag(stateInfo, settings.module.body.mass);
-
-        /*4. Calculate the total resultant change in position by gravity and drag*/
-        ArrayList<Vector2d> cpT = new ArrayList<Vector2d>();
-        for(int i=0; i< stateInfo.position.size(); i++)
-        {
-            cpT.add(stateInfo.velocity.get(i));
-        }
-        
-        /* 5.Return rateInterface object reflecting these changes*/
-        return new Rate2d(cvT, cpT);
+        /* 3.Return rateInterface object reflecting these changes*/
+        return new Rate2d(cv, cp);
     }
 	
 	/**
@@ -54,7 +47,7 @@ public class FreeFallFunction implements ODEFunctionInterface {
      * @param masses Array containing all masses of the Celestial Bodies in the system
      * @return An array containing resultant acceleration of all Celestial bodies in the system
     */
-    public ArrayList<Vector2d> newtonGravity(State2d state, double planetMass)
+    public ArrayList<Vector2d> freeFall(State2d state, double planetMass)
     {
         /*Initial*/
     	State2d stateInfo = (State2d) state;													//Cast into State object to access information                   
@@ -74,19 +67,35 @@ public class FreeFallFunction implements ODEFunctionInterface {
              double r = otherPos.dist(currentPos);                                               //Calculate the distance between the two planets
 
              /*Unit vector calc*/
-             Vector2d rHat = (otherPos.sub(currentPos)).unitVector();
-
+             Vector2d rHat = (otherPos.sub(settings.cbLocation)).unitVector();
+             Vector2d unitVec = (otherPos.sub(settings.cbLocation)).unitVector();
+             
              /*Scaler quantity calc */
              double quantity = (GRAVITY*planetMass*otherMass)/ Math.pow(r,2);
 
              /*Gravitational force calc */
              Vector2d gravitationalForce = rHat.mul(quantity);                                   //Direction vector multiplied by non-vector quantity
 
-             /*Resultant acceleration calc*/
-             Vector2d resAcceleration = gravitationalForce.mul(1.0/planetMass);                  //a = F/mass 
-
+            //drag coefficient * area * air density * 0.5
+     		double dScale = r*dragCoeff*area*0.5;
+     		
+     		//vector^2
+     		double vSq = stateInfo.velocity.get(j).dotProduct(stateInfo.velocity.get(j)); 		//WRONG, FOLLOW NASA THING MORE CAREFULLY!!!!
+     		
+     		//NASA drag formula
+     		double dragFormula = dScale*vSq;							//follow the NASA given formula to get the mathematical force applied by drag
+     		
+     		//drag force being applied to landing module
+     		Vector2d dragForce = unitVec.mul(1/dragFormula);			//convert the given drag force into vector form
+     		
+     		//resultant force
+     		Vector2d resForce = gravitationalForce.sub(dragForce);
+     		
+     		//resultant acceleration due to force
+    		Vector2d resAccel = dragForce.mul(1/settings.module.body.mass);				//a = F/mass
+     		
              /*Summation*/
-             accelerationSum  = accelerationSum.add(resAcceleration);
+             accelerationSum  = accelerationSum.add(resAccel);
                 
             }
             changeInVelocity.add(accelerationSum);                                                   //Record accelerationSum / change in velocity
@@ -95,45 +104,5 @@ public class FreeFallFunction implements ODEFunctionInterface {
         return changeInVelocity;
     }
     
-    /**
-     * newtonGravity method calculates Celestial Body acceleration as a result of gravitational relations between all Celestial bodies
-     * @param y The state of the current system.
-     * @param masses Array containing all masses of the Celestial Bodies in the system
-     * @return An array containing resultant acceleration of all Celestial bodies in the system
-    */
-    public ArrayList<Vector2d> drag(State2d state, double planetMass)
-    {
-    	
-    	State2d stateInfo = (State2d) state;													//Cast into State object to access information                   
-        ArrayList<Vector2d> changeInVelocity = new ArrayList<Vector2d>();
-        
-        Vector2d currentPos = stateInfo.position.get(0);
-        Vector2d accelerationSum = new Vector2d(0,0);   
-        
-        double r = 147000;
-		double dragCoeff = 0.55;
-		double area = settings.module.length * settings.module.width;
-        
-    	for (int i = 0; i < stateInfo.velocity.size(); i++) {
-    		
-    		Vector2d modulePos = stateInfo.position.get(i);
-    		
-    		Vector2d unitVec = (modulePos.sub(settings.cbLocation)).unitVector();
-    		
-    		//drag coeff*area*air density* 0.5
-    		double dScale = r*dragCoeff*area*0.5;
-    		Vector2d dForce = unitVec.mul(dScale);
-    		
-    		//resultant acceleration due to force
-    		Vector2d dAccel = dForce.mul(1/settings.module.body.mass);
-    		
-    		accelerationSum.add(dAccel);
-    	}
-    	changeInVelocity.add(accelerationSum);
-    	
-    	return changeInVelocity;
-    	
-    }
-    
-    
+
 }
