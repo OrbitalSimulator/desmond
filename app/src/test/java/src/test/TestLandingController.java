f39055f3f61be53f2552df2bfa0cf4a19eeb4886
Jerome <jerome@cjpr.be>
2021-06-18 15:06:33 +0200

    test added for impact method

--- app/src/test/java/src/test/TestLandingController.java
+++ app/src/test/java/src/test/TestLandingController.java
@@ -26,10 +26,126 @@ class TestLandingController
 		lc.plotTrajectory(landerPos, landerVel, landerMass, titanPos, titanVel, titanMass, titanRadius);
 		
 	}
-	
-	@Test void testImpact()
+	/**
+	 * 	Testing Strategy:
+	 *  	|  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 |
+	 *  x   | >0 | <0 | >0 | <0 |  0 |  0 |  0 |  0 | >0 | >0 | >0 | >0 | <0 | <0 | <0 | <0 |
+	 *  y   |  0 |  0 |  0 |  0 | >0 | <0 | >0 | <0 | <0 | >0 | <0 | >0 | <0 | >0 | <0 | >0 |
+	 * d<=r |  T |  T |  F |  F |  T |  T |  F |  F |  T |  T |  F |  F |  T |  T |  F |  F |
+	 */
+	// TODO (Jerome) 4hrs
+	@Test void testImpact1()
 	{
-		// TODO (Jerome) 4hrs
+		Vector3d v = new Vector3d(35.0, 0.0, 0.0);
+		double r = 50.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact2()
+	{
+		Vector3d v = new Vector3d(-20, 0.0, 0.0);
+		double r = 20.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact3()
+	{
+		Vector3d v = new Vector3d(60.0, 0.0, 0.0);
+		double r = 45.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact4()
+	{
+		Vector3d v = new Vector3d(-40.0, 0.0, 0.0);
+		double r = 30.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact5()
+	{
+		Vector3d v = new Vector3d(0.0, 60, 0.0);
+		double r = 60.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact6()
+	{
+		Vector3d v = new Vector3d(0.0, -20.0, 0.0);
+		double r = 25.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	
+	@Test void testImpact7()
+	{
+		Vector3d v = new Vector3d(0.0, 35.0, 0.0);
+		double r = 25.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact8()
+	{
+		Vector3d v = new Vector3d(0.0, -40.0, 0.0);
+		double r = 30.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact9()
+	{
+		Vector3d v = new Vector3d(50.0, -30.0, 0.0);
+		double r = 58.30951895;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact10()
+	{
+		Vector3d v = new Vector3d(15.0, 20.0, 0.0);
+		double r = 25.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact11()
+	{
+		Vector3d v = new Vector3d(30.0, -15.0, 0.0);
+		double r = 30.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact12()
+	{
+		Vector3d v = new Vector3d(31.0, 46.0, 0.0);
+		double r = 40.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact13()
+	{
+		Vector3d v = new Vector3d(-20.0, -15.0, 0.0);
+		double r = 25.0;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact14()
+	{
+		Vector3d v = new Vector3d(-30.0, 20.0, 0.0);
+		double r = 37;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), true);
+	}
+	@Test void testImpact15()
+	{
+		Vector3d v = new Vector3d(-40.0, -30.0, 0.0);
+		double r = 30;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
+	}
+	@Test void testImpact16()
+	{
+		Vector3d v = new Vector3d(-20.0, 60.0, 0.0);
+		double r = 60;
+		LandingController lc = new LandingController();
+		assertEquals(lc.impact(v, r), false);
 	}
 	
 	@Test void testDrag()
