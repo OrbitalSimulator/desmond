7cb2b006d5c7e07993d8e6329a4af3799bac6a99
Mustafa Alp Erçıkan <72265922+ercikanalp@users.noreply.github.com>
2021-06-21 19:49:02 +0300

    Implemented getAngle method.

--- app/src/main/java/src/land/ClosedLoopController.java
+++ app/src/main/java/src/land/ClosedLoopController.java
@@ -55,4 +55,27 @@ public class ClosedLoopController extends LandingController
 	
 	return trajectory;
 	}
+
+	public double getAngle(Vector3d velocity, Vector3d landLocation, Vector3d probeLocation)
+	{
+		double a = distance(landLocation,velocity);
+		double b = distance(probeLocation,landLocation);
+		double c = distance(probeLocation,velocity);
+		double cosA= cosineLaw(a,b,c);
+		return cosA;
+	}
+
+	private double cosineLaw(double a, double b, double c)
+	{
+		// cos(A) = (b^2 + c^2 - a^2) / ( 2*b*c)
+		double value= Math.acos(Math.pow(b,2)/(2*b*c) + Math.pow(c,2)/(2*b*c) - Math.pow(a,2)/(2*b*c));
+		return Math.toDegrees(value);
+	}
+
+	private double distance(Vector3d a, Vector3d b)
+	{
+		return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2)+ Math.pow(a.getZ() - b.getZ(),2));
+	}
+
+
 }
